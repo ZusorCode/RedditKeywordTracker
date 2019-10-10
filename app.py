9a491b5ff04782a9aeddc9041cfb25a4206132fb
Tobias Messner <tobias.d.messner@gmail.com>
2019-10-10 22:09:53 +0200

    Fixed issue with data missing and server erroring

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ db = redis.Redis(host=config_data.redis_host, port=6379, db=0)
 def get_titles():
     if db.get("enabled"):
         keys = sorted(db.keys())
-        key_name = keys[len(keys)-1]
+        key_name = keys[0]
         return pickle.loads(db.get(key_name))
 
 
@@ -25,10 +25,14 @@ def main():
 @app.route("/api/<string:keyword>")
 def api(keyword):
     counter = 0
-    for post in get_titles():
-        if keyword.lower() in post.lower():
-            counter += 1
-    return jsonify({"count": counter})
+    titles = get_titles()
+    if titles:
+        for post in titles:
+            if keyword.lower() in post.lower():
+                counter += 1
+        return jsonify({"count": counter})
+    else:
+        return jsonify({"count": 0})
 
 
 if __name__ == '__main__':
